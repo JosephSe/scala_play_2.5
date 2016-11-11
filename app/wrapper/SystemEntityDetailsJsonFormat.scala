e516234f60d8579dc241440bf18162307bca0c0f
Ilavarasi Sugumar <isugum@sapient.com>
2016-11-11 12:49:25 +0530

    corrected typo

--- app/wrapper/SystemEntityDetailsJsonFormat.scala
+++ app/wrapper/SystemEntityDetailsJsonFormat.scala
@@ -6,7 +6,7 @@ import play.api.libs.json.Json
 /**
   * Created by isugum on 11/8/2016.
   */
-case class SystemEntityDetailsJsonFormat(category: String, cohproperties: Map[String, Long], atghproperties: Map[String, Long], gcproperties: Map[String, Long])
+case class SystemEntityDetailsJsonFormat(category: String, cohproperties: Map[String, Long], atgproperties: Map[String, Long], gcproperties: Map[String, Long])
 
 object SystemEntityDetailsJsonFormat {
   implicit val systemEntityDetailsJsonFormat = Json.format[SystemEntityDetailsJsonFormat]
